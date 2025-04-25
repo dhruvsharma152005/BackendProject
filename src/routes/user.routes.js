be1619155be3ccdebfbae7edeaee40ed324f19b2
Rj15-collab <dkdsharma15@gmail.com>
2025-04-25 16:09:41 +0530

    final show

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,5 +1,5 @@
 import {Router} from "express";
-import { logoutUser, registerUser,loginUser,refreshAccessToken } from "../controllers/user.controller.js";
+import { logoutUser, registerUser,loginUser,refreshAccessToken, changeCurrentPassword, getCurrentUser, updateAccountDetails, updateUserAvatar, updateUserCoverImage, getWatchHistory } from "../controllers/user.controller.js";
 import { upload } from "../middlewares/multer.middleware.js";
 const router = Router();
 import { verifyJWT } from "../middlewares/auth.middleware.js";
@@ -28,5 +28,17 @@ router.route("/logout").post(verifyJWT, logoutUser) //verifyJwt middleware
 
 router.route("/refresh-token").post(refreshAccessToken)
 
+router.route("/change-password").post(verifyJWT,changeCurrentPassword)  //verifyjwt means only logged in log hi kr paye  use badh method call
+//means logged walo ke liye
+router.route("/current-user").get(verifyJWT,getCurrentUser)
 
+router.route("/update-account").patch(verifyJWT,updateAccountDetails)  //post meh saaridetail update ho jati ha
+
+router.route("/avatar").patch(verifyJWT,upload.single("avatar"),updateUserAvatar)
+
+router.route("/cover-image").patch(verifyJWT,upload.single("/coverImage"),updateUserCoverImage)
+
+router.route("/c/:username").get(verifyJWT,getUserChannelProfile)
+
+router.route("/history").get(verifyJWT,getWatchHistory)
 export default router;
